4fe2662862fe5bb82a12e966e9ee18cb160c1ed8
vvicunag <vvicuna1@uc.cl>
2022-01-06 14:30:07 +0100

    add sequelize and mysql2 database connection

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Dependencies
 const express = require("express");
-// Import express-handlebars
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const path = require("path");
+const sequelize = require("./config/connection");
 
 // Sets up the Express App
 const app = express();
@@ -12,10 +12,15 @@ const PORT = process.env.PORT || 3535;
 //setting Handlebars.js as the default template engine.
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers/index"));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+// Starts database and the server to begin listening
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
