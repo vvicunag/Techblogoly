939e568c20c48df2385fefd361b3c6d6b9c5c23c
vvicunag <vvicuna1@uc.cl>
2022-01-18 08:05:22 +0100

    add envExample, add post rendering on home

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
+const { Post, Comment } = require("../models");
 
 // GET route main
 router.get("/", async (req, res) => {
   // This method is rendering the 'home' Handlebars.js template
-  res.render("home");
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: Comment, attributes: ["comment_content"] }],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("home", { posts });
+  } catch {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
