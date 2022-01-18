8805b193363d88d23a9f29fadc1054df6174e7c5
vvicunag <vvicuna1@uc.cl>
2022-01-18 08:24:43 +0100

    render comments on home

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
-const { Post, Comment } = require("../models");
+const { Post, Comment, User } = require("../models");
 
 // GET route main
 router.get("/", async (req, res) => {
   // This method is rendering the 'home' Handlebars.js template
   try {
     const postData = await Post.findAll({
-      include: [{ model: Comment, attributes: ["comment_content"] }],
+      include: [
+        {
+          model: Comment,
+          attributes: ["comment_content", "user_id", "created_on"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
     });
     const posts = postData.map((post) => post.get({ plain: true }));
     console.log(posts);
