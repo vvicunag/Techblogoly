53c10776a4c2150caca4ba910d3a669a76b2f230
vvicunag <vvicuna1@uc.cl>
2022-01-28 15:43:27 +0100

    render dash and start handelbar

--- controllers/private-routes.js
+++ controllers/private-routes.js
@@ -3,10 +3,27 @@ const { Post, Comment, User } = require("../models");
 
 //router.use("/dashboard",dashboardRouter);
 
-router.get("/dashboard", (req, res) => {
-    res.render("dashboard", {
-        loggedIn: req.session.loggedIn,
-    });
+router.get("/dashboard", async (req, res) => {
+    try{
+      //const id = req.session.userId;
+      const id = 1
+      const dashboardData = await User.findByPk(id, {
+        attributes: { exclude: ['password'] },
+        include: [{model: Post}],
+      });
+      userData = dashboardData.dataValues;
+      console.log(userData);
+      //dashboardData = dashboardData.get({plain: true});
+      //dashboardData = dashboardData.dataValues;
+      res.render("dashboard", {
+          loggedIn: req.session.loggedIn,
+          userData,
+      });
+    }
+    catch {
+    return res.status(500).json({ error: "Failed to load dashboard" });
+    }
   });
 
 module.exports = router;
+
